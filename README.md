46e3e9a96f66676fe919813a2e3595bf1430b478
Sarah Ragowski <Sarah.Ragowski@stud.fra-uas.de>
2022-03-16 21:30:53 +0100

    Add the README file and reformatted the files.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # CC_FaceRecognition
+
+The target application to deploy on OpenShift and the IBM Cloud is a web application for face recognition. It can be
+used due a web interface or an API.
+
+The REST API, that is also used from the web interface, provides two routes:
+
+GET `/` <br />
+Returns the index.html for the web interface.
+
+POST `/response` <br />
+The request body must provide an image. The response contains the given image with a marking for all the detected faces
+in it.
--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import os
-from flask import Flask, request, redirect, url_for, send_from_directory
-from flask import render_template
-import sys
+
 import cv2
+from flask import Flask, request, send_from_directory
+from flask import render_template
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-
 app.config["UPLOAD_FOLDER"] = 'images'
+
+cascade_path = "templates/haarcascade_frontalface_default.xml"
 
 
 @app.route('/')
@@ -24,26 +25,26 @@ def recognize_image():
     new_imgname = face_detect(filepath, filename)
     return send_from_directory(app.config['UPLOAD_FOLDER'], new_imgname)
 
-
-def face_detect(imgpath, imgname, cascasdepath = "templates/haarcascade_frontalface_default.xml"):
+'''
+Method to detect the containing faces for the given image. For this OpenCV and a cascade classifier for faces is used.
+Return the name of the new image with the marked faces.
+'''
+def face_detect(imgpath, imgname):
     image = cv2.imread(imgpath)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image_grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     new_imgname = f"FR_{imgname}"
 
-    face_cascade = cv2.CascadeClassifier(cascasdepath)
-
+    face_cascade = cv2.CascadeClassifier(cascade_path)
     faces = face_cascade.detectMultiScale(
-        gray,
-        scaleFactor = 1.1,
-        minNeighbors = 5,
-        minSize = (30,30)
-        )
+        image_grayscale,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
 
-    print("The number of faces found = ", len(faces))
-
-    for (x,y,w,h) in faces:
-        cv2.rectangle(image, (x,y), (x+h, y+h), (0, 255, 0), 2)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x + h, y + h), (0, 255, 0), 2)
 
     os.chdir(app.config["UPLOAD_FOLDER"])
     cv2.imwrite(new_imgname, image)
@@ -54,5 +55,3 @@ def face_detect(imgpath, imgname, cascasdepath = "templates/haarcascade_frontalf
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8080')
-
-
